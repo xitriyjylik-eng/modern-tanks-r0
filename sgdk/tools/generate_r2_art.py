6ebd952febbc29dec71f0278f7b00f2a1f8c804c
xitriyjylik-eng <xitriyjylik@gmail.com>
2026-09-08 15:03:46 +1000

    Use four exact small leading background chunks

--- sgdk/tools/generate_r2_art.py
+++ sgdk/tools/generate_r2_art.py
@@ -22,8 +22,10 @@ def decode_text_payload(parts, out_name):
 
 
 bg_path = decode_text_payload([
-    "r2_menu_bg.png.b64.000a0",
-    "r2_menu_bg.png.b64.000a1",
+    "r2_menu_bg.png.b64.000p0",
+    "r2_menu_bg.png.b64.000p1",
+    "r2_menu_bg.png.b64.000p2",
+    "r2_menu_bg.png.b64.000p3",
     "r2_menu_bg.png.b64.000b2",
     "r2_menu_bg.png.b64.000c",
     "r2_menu_bg.png.b64.001",
