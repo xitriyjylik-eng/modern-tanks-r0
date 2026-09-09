5c15e7c151ed1067eb78c79690f35851efb9e0c5
xitriyjylik-eng <xitriyjylik@gmail.com>
2026-09-09 12:16:09 +1000

    R3: add battle renderer interface

---
+++ sgdk/inc/battle_r3.h
@@ -0,0 +1,19 @@
+#ifndef _BATTLE_R3_H_
+#define _BATTLE_R3_H_
+
+#include <genesis.h>
+
+#define R3_WORLD_WIDTH_PX 8192
+#define R3_WORLD_HEIGHT_PX 8192
+#define R3_BATTLEFIELD_WIDTH_PX 224
+#define R3_BATTLEFIELD_HEIGHT_PX 192
+#define R3_HUD_WIDTH_PX 96
+#define R3_LOG_HEIGHT_PX 32
+
+void R3_battleEnter(void);
+void R3_battleLeave(void);
+void R3_battleSetInput(u16 held, u16 pressed);
+void R3_battleUpdate(void);
+bool R3_pointInsideBattlefield(s16 screenX, s16 screenY);
+
+#endif
