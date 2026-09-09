ea2e8ff8b9d1d2e08187772b337809041d734726
xitriyjylik-eng <xitriyjylik@gmail.com>
2026-09-10 08:38:28 +1000

    Use 40 raw chunks in R3 streaming renderer

--- sgdk/src/r3_stream_world.c
+++ sgdk/src/r3_stream_world.c
@@ -33,7 +33,7 @@ static s32 cameraTargetYQ8 = 320L << R3_CAM_FP_SHIFT;
 static s16 loadedBaseTileX = 0;
 static s16 loadedBaseTileY = 0;
 
-static const u8 * const r3TileChunks[30] =
+static const u8 * const r3TileChunks[40] =
 {
     r3_target_world_tiles_00,
     r3_target_world_tiles_01,
@@ -64,7 +64,17 @@ static const u8 * const r3TileChunks[30] =
     r3_target_world_tiles_26,
     r3_target_world_tiles_27,
     r3_target_world_tiles_28,
-    r3_target_world_tiles_29
+    r3_target_world_tiles_29,
+    r3_target_world_tiles_30,
+    r3_target_world_tiles_31,
+    r3_target_world_tiles_32,
+    r3_target_world_tiles_33,
+    r3_target_world_tiles_34,
+    r3_target_world_tiles_35,
+    r3_target_world_tiles_36,
+    r3_target_world_tiles_37,
+    r3_target_world_tiles_38,
+    r3_target_world_tiles_39
 };
 
 static s32 clampQ8(s32 value, s32 loPx, s32 hiPx)
@@ -81,9 +91,7 @@ static s32 smoothCameraAxis(s32 current, s32 target)
     s32 delta = target - current;
     s32 step;
 
-    if ((delta > -R3_CAM_SNAP_Q8) && (delta < R3_CAM_SNAP_Q8))
-        return target;
-
+    if ((delta > -R3_CAM_SNAP_Q8) && (delta < R3_CAM_SNAP_Q8)) return target;
     step = delta >> R3_CAM_RESPONSE_SHIFT;
     if (step == 0) step = (delta > 0) ? 1 : -1;
     if (step > R3_CAM_MAX_STEP_Q8) step = R3_CAM_MAX_STEP_Q8;
@@ -110,48 +118,40 @@ static void loadWorldTile(s16 worldX, s16 worldY, TransferMethod tm)
     const u16 wx = clampWorldTileX(worldX);
     const u16 wy = clampWorldTileY(worldY);
     const u32 worldIndex = ((u32) wy * R3_WORLD_W_TILES) + wx;
-    const u16 chunk = wy / 4;
-    const u16 localRow = wy % 4;
+    const u16 chunk = wy / 3;
+    const u16 localRow = wy % 3;
     const u32 localIndex = ((u32) localRow * R3_WORLD_W_TILES) + wx;
-
     const u16 slotX = ((u16) worldX) & (R3_RING_W - 1);
     const u16 slotY = ((u16) worldY) & (R3_RING_H - 1);
     const u16 planeX = ((u16) worldX) & (R3_PLANE_W - 1);
     const u16 planeY = ((u16) worldY) & (R3_PLANE_H - 1);
-
     const u16 patternIndex = R3_PATTERN_BASE + (slotY * R3_RING_W) + slotX;
     const u8 palette = r3_target_world_banks[worldIndex] & 3;
     const u8 *pattern = r3TileChunks[chunk] + (localIndex * 32UL);
 
     VDP_loadTileData((const u32 *) pattern, patternIndex, 1, tm);
-    VDP_setTileMapXY(BG_B,
-                     TILE_ATTR_FULL(palette, FALSE, FALSE, FALSE, patternIndex),
-                     planeX, planeY);
+    VDP_setTileMapXY(BG_B, TILE_ATTR_FULL(palette, FALSE, FALSE, FALSE, patternIndex), planeX, planeY);
 }
 
 static void loadColumn(s16 worldX, s16 baseY, TransferMethod tm)
 {
     s16 y;
-    for (y = 0; y < R3_RING_H; y++)
-        loadWorldTile(worldX, baseY + y, tm);
+    for (y = 0; y < R3_RING_H; y++) loadWorldTile(worldX, baseY + y, tm);
 }
 
 static void loadRow(s16 baseX, s16 worldY, TransferMethod tm)
 {
     s16 x;
-    for (x = 0; x < R3_RING_W; x++)
-        loadWorldTile(baseX + x, worldY, tm);
+    for (x = 0; x < R3_RING_W; x++) loadWorldTile(baseX + x, worldY, tm);
 }
 
 static void loadInitialRing(s16 baseX, s16 baseY)
 {
     s16 y;
     s16 x;
-
     for (y = 0; y < R3_RING_H; y++)
         for (x = 0; x < R3_RING_W; x++)
             loadWorldTile(baseX + x, baseY + y, CPU);
-
     loadedBaseTileX = baseX;
     loadedBaseTileY = baseY;
 }
@@ -163,19 +163,16 @@ static void updateStreaming(s16 tileX, s16 tileY)
         loadColumn(loadedBaseTileX + R3_RING_W, loadedBaseTileY, CPU);
         loadedBaseTileX++;
     }
-
     while (tileX < loadedBaseTileX)
     {
         loadedBaseTileX--;
         loadColumn(loadedBaseTileX, loadedBaseTileY, CPU);
     }
-
     while (tileY > loadedBaseTileY)
     {
         loadRow(loadedBaseTileX, loadedBaseTileY + R3_RING_H, CPU);
         loadedBaseTileY++;
     }
-
     while (tileY < loadedBaseTileY)
     {
         loadedBaseTileY--;
@@ -187,36 +184,20 @@ static void drawHud(void)
 {
     VDP_fillTileData(0x77, R3_HUD_BG_TILE, 1, TRUE);
     VDP_fillTileData(0x11, R3_HUD_SOLID_TILE, 1, TRUE);
-
     VDP_clearPlane(BG_A, TRUE);
-
-    VDP_fillTileMapRect(BG_A,
-        TILE_ATTR_FULL(PAL0, TRUE, FALSE, FALSE, R3_HUD_BG_TILE),
-        28, 0, 12, 24);
-
-    VDP_fillTileMapRect(BG_A,
-        TILE_ATTR_FULL(PAL0, TRUE, FALSE, FALSE, R3_HUD_BG_TILE),
-        0, 24, 40, 4);
-
+    VDP_fillTileMapRect(BG_A, TILE_ATTR_FULL(PAL0, TRUE, FALSE, FALSE, R3_HUD_BG_TILE), 28, 0, 12, 24);
+    VDP_fillTileMapRect(BG_A, TILE_ATTR_FULL(PAL0, TRUE, FALSE, FALSE, R3_HUD_BG_TILE), 0, 24, 40, 4);
     VDP_setTextPlane(BG_A);
     VDP_setTextPalette(PAL0);
     VDP_setTextPriority(TRUE);
-
     VDP_drawText("T-1", 30, 1);
     VDP_drawText("HP", 29, 4);
     VDP_drawText("ARMOR", 29, 7);
     VDP_drawText("AMMO", 29, 10);
     VDP_drawText("SCORE", 29, 14);
     VDP_drawText("002480", 29, 16);
-
-    VDP_fillTileMapRect(BG_A,
-        TILE_ATTR_FULL(PAL2, TRUE, FALSE, FALSE, R3_HUD_SOLID_TILE),
-        32, 4, 6, 1);
-
-    VDP_fillTileMapRect(BG_A,
-        TILE_ATTR_FULL(PAL1, TRUE, FALSE, FALSE, R3_HUD_SOLID_TILE),
-        32, 7, 5, 1);
-
+    VDP_fillTileMapRect(BG_A, TILE_ATTR_FULL(PAL2, TRUE, FALSE, FALSE, R3_HUD_SOLID_TILE), 32, 4, 6, 1);
+    VDP_fillTileMapRect(BG_A, TILE_ATTR_FULL(PAL1, TRUE, FALSE, FALSE, R3_HUD_SOLID_TILE), 32, 7, 5, 1);
     VDP_drawText("B: MENU", 1, 25);
     VDP_drawText("A/C: FAST", 15, 25);
 }
@@ -230,15 +211,11 @@ void R3_worldEnter(void)
 
     VDP_setPlaneSize(R3_PLANE_W, R3_PLANE_H, TRUE);
     VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
-
     VDP_clearPlane(BG_A, TRUE);
     VDP_clearPlane(BG_B, TRUE);
-
     PAL_setColors(0, r2_menu_bg.palette->data, 64, CPU);
-
     loadInitialRing(tileX, tileY);
     drawHud();
-
     VDP_setHorizontalScroll(BG_A, 0);
     VDP_setVerticalScroll(BG_A, 0);
     VDP_setHorizontalScroll(BG_B, -cameraX);
@@ -269,17 +246,13 @@ void R3_worldUpdate(u16 held)
 
     cameraTargetXQ8 = clampQ8(cameraTargetXQ8, 0, R3_WORLD_W_PX - R3_VIEW_W_PX);
     cameraTargetYQ8 = clampQ8(cameraTargetYQ8, 0, R3_WORLD_H_PX - R3_VIEW_H_PX);
-
     cameraPosXQ8 = smoothCameraAxis(cameraPosXQ8, cameraTargetXQ8);
     cameraPosYQ8 = smoothCameraAxis(cameraPosYQ8, cameraTargetYQ8);
-
     cameraX = (s16)(cameraPosXQ8 >> R3_CAM_FP_SHIFT);
     cameraY = (s16)(cameraPosYQ8 >> R3_CAM_FP_SHIFT);
     tileX = cameraX >> 3;
     tileY = cameraY >> 3;
-
     updateStreaming(tileX, tileY);
-
     VDP_setHorizontalScroll(BG_B, -cameraX);
     VDP_setVerticalScroll(BG_B, cameraY);
 }
